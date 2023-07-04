cf8c94945c613a1e6d604810d967a064a0d6ebce
Metehan Gezer <irrelevantdev@outlook.com>
2023-07-04 10:45:03 +0300

    Add missing libpng for x86 This might not be correct

---
+++ libs/libpng/x86/objs/png/arm/arm_init.o.d
@@ -0,0 +1,17 @@
+.//obj/local/x86_64/objs/png/arm/arm_init.o: jni/arm/arm_init.c \
+  jni/arm/../pngpriv.h jni/pnglibconf.h jni/png.h jni/pngconf.h \
+  jni/pngstruct.h jni/pnginfo.h jni/pngdebug.h
+
+jni/arm/../pngpriv.h:
+
+jni/pnglibconf.h:
+
+jni/png.h:
+
+jni/pngconf.h:
+
+jni/pngstruct.h:
+
+jni/pnginfo.h:
+
+jni/pngdebug.h:
---
+++ libs/libpng/x86/objs/png/arm/filter_neon_intrinsics.o.d
@@ -0,0 +1,17 @@
+.//obj/local/x86_64/objs/png/arm/filter_neon_intrinsics.o: \
+  jni/arm/filter_neon_intrinsics.c jni/arm/../pngpriv.h jni/pnglibconf.h \
+  jni/png.h jni/pngconf.h jni/pngstruct.h jni/pnginfo.h jni/pngdebug.h
+
+jni/arm/../pngpriv.h:
+
+jni/pnglibconf.h:
+
+jni/png.h:
+
+jni/pngconf.h:
+
+jni/pngstruct.h:
+
+jni/pnginfo.h:
+
+jni/pngdebug.h:
